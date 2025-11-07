dadf0f2d1f9aad217f6ff4c15cf1ee886be3aba0
Dorothy-S <Dorothy3467@gmail.com>
2025-11-07 18:49:42 -0500

    new file:   routes/index.js

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express'); 
+var router = express.Router();
+
+/* home page. */
+router.get('/', function(req, res, next) {
+  res.render('pages/home', { title: 'Dorothy Sahijwani', activePage: 'home' });
+});
+
+/* home page. */
+router.get('/home', function(req, res, next) {
+  res.render('pages/home', { title: 'Dorothy Sahijwani', activePage: 'home' });
+});
+
+/* About page. */
+router.get('/about', function(req, res, next) {
+  res.render('pages/about', { title: 'About Me', activePage: 'about' });
+});
+
+/* projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('pages/projects', { title: 'Projects', activePage: 'projects' });
+});
+
+/* contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('pages/contact', { title: 'Contact Me', activePage: 'contact' });
+});
+
+module.exports = router; 
+ 
