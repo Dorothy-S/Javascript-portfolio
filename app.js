408a30d09d7595a31df7d20fa2730dc9c87b4da0
Dorothy <116375877+Dorothy-S@users.noreply.github.com>
2025-11-07 19:26:48 -0500

    Remove comments from app.js
    Removed comments for clarity in app.js.

--- app.js
+++ app.js
@@ -3,18 +3,14 @@ const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
 const app = express();
 
-// Set EJS as templating engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// Enable layouts
 app.use(expressLayouts);
 app.set('layout', 'layout');
 
-// Serve static files from public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
 const indexRouter = require('./routes/index');
 app.use('/', indexRouter);
 
